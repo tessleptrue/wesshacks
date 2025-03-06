f7fafcebeebd027047a961e0c1a736427c9dc978
Chase Yahn-Krafft <cyahnkrafft@wesleyan.edu>
2025-03-06 16:49:32 -0500

    Now works on local env

--- config.php
+++ config.php
@@ -1,4 +1,9 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+?>
+
+<?php
 // Database configuration
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');     // Replace with your MySQL username
